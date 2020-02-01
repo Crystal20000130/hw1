5dbfcacb23ebe18c560aa66a482574f92e6a1bf2
Crystal20000130 <60421925+Crystal20000130@users.noreply.github.com>
2020-01-31 23:48:39 -0800

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -6,21 +6,21 @@ function setup() {
   createCanvas(500, 500);
   x = random(width);
   y = random(height);
-  radius = 60;
+  radius = 80;
 }
 
 function draw() {
-  background(700);
+  background(200);
 
   if (dist(mouseX, mouseY, x, y) < radius) {
     if (mouseIsPressed) {
       x = random(width);
       y = random(height);
     }
-    fill(100, 200, 300, 300);
+    fill(100, 600, 300, 300);
   }
   else {
-    fill(250, 220, 200, 250);
+    fill(200, 220, 200, 250);
   }
 
   ellipse(x, y, radius * 2);
